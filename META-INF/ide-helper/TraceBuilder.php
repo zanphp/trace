a03dbf6c25c29a085ee4c5dcdf3c1c8d1a6eb4d1
huye <huye@youzan.com>
2017-09-05 19:47:57 +0800

    优化命名空间，支持IDE跳转

--- META-INF/ide-helper/TraceBuilder.php
+++ META-INF/ide-helper/TraceBuilder.php
@@ -5,38 +5,45 @@ namespace Zan\Framework\Sdk\Trace;
 
 class TraceBuilder
 {
+    private $TraceBuilder;
+
+    public function __construct()
+    {
+        $this->TraceBuilder = new \ZanPHP\Trace\TraceBuilder();
+    }
+
     public function buildHeader(array $header)
     {
-
+        $this->TraceBuilder->buildHeader($header);
     }
 
     public function buildTransaction(array $transaction)
     {
-
+        $this->TraceBuilder->buildTransaction($transaction);
     }
 
     public function commitTransaction(array $transaction)
     {
-
+        $this->TraceBuilder->commitTransaction($transaction);
     }
 
     public function buildEvent(array $event)
     {
-
+        $this->TraceBuilder->buildEvent($event);
     }
 
     public function isNotEmpty()
     {
-
+        $this->TraceBuilder->isNotEmpty();
     }
 
     public function getData()
     {
-
+        $this->TraceBuilder->getData();
     }
 
     public static function generateId()
     {
-
+        \ZanPHP\Trace\TraceBuilder::generateId();
     }
 }
